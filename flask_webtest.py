7109674cb9bbcbcb926bfe0340982cfc1a13bced
Anton Romanovich <anthony.romanovich@gmail.com>
2013-06-08 02:10:14 +0600

    Refactor internals

--- flask_webtest.py
+++ flask_webtest.py
@@ -5,7 +5,7 @@ from functools import partial
 from webtest import (TestApp as BaseTestApp,
                      TestRequest as BaseTestRequest,
                      TestResponse as BaseTestResponse)
-from flask import session, get_flashed_messages
+from flask import g, session, get_flashed_messages
 from flask.signals import template_rendered, request_started, request_finished
 try:
     # Available starting with Flask 0.10
@@ -14,40 +14,32 @@ except ImportError:
     message_flashed = None
 
 
-def store_rendered_template(store, app, template, context, **extra):
-    store.setdefault('contexts', []).append((template.name, context))
+def store_rendered_template(app, template, context, **extra):
+    g._flask_webtest.setdefault('contexts', []).append((template.name, context))
 
 
-def store_flashed_message(store, app, message, category, **extra):
-    store.setdefault('flashes', []).append((category, message))
+def store_flashed_message(app, message, category, **extra):
+    g._flask_webtest.setdefault('flashes', []).append((category, message))
 
 
-def store_session(store, app, response, **extra):
-    store['session'] = dict(session)
-
-
-def add_context_processor(store, app):
-    """Adds context processor that injects modified version of
-    `get_flashed_messages` which stores consumed messages in `store`.
-    """
-    def flask_webtest_hook():
-        def wrapper(*args, **kwargs):
-            # `get_flashed_messages` removes messages from session
+def set_up(app, *args, **extra):
+    g._flask_webtest = {}
+    if not message_flashed:
+        def _get_flashed_messages(*args, **kwargs):
+            # `get_flashed_messages` removes messages from session,
+            # so we store them in `g._flask_webtest`
             flashes_to_be_consumed = copy(session.get('_flashes', []))
-            store.setdefault('flashes', []).extend(flashes_to_be_consumed)
+            g._flask_webtest.setdefault('flashes', []).extend(flashes_to_be_consumed)
             return get_flashed_messages(*args, **kwargs)
-        return {'get_flashed_messages': wrapper}
-    app.template_context_processors[None].append(flask_webtest_hook)
+        app.jinja_env.globals['get_flashed_messages'] = _get_flashed_messages
 
 
-def remove_context_processor(app, response, **extra):
-    processor_to_be_removed = None
-    for processor in app.template_context_processors[None]:
-        if getattr(processor, 'func_name', None) == 'flask_webtest_hook':
-            processor_to_be_removed = processor
-
-    if processor_to_be_removed:
-        app.template_context_processors[None].remove(processor_to_be_removed)
+def tear_down(store, app, response, *args, **extra):
+    g._flask_webtest['session'] = dict(session)
+    store.update(g._flask_webtest)
+    del g._flask_webtest
+    if not message_flashed:
+        app.jinja_env.globals['get_flashed_messages'] = get_flashed_messages
 
 
 class TestResponse(BaseTestResponse):
@@ -77,28 +69,22 @@ class TestApp(BaseTestApp):
 
     def do_request(self, *args, **kwargs):
         store = {}
-        store_rendered_template_ = partial(store_rendered_template, store)
-        store_session_ = partial(store_session, store)
+        tear_down_ = partial(tear_down, store)
 
-        template_rendered.connect(store_rendered_template_)
-        request_finished.connect(store_session_)
+        request_started.connect(set_up)
+        request_finished.connect(tear_down_)
+        template_rendered.connect(store_rendered_template)
         if message_flashed:
             message_flashed.connect(store_flashed_message)
-        else:
-            add_context_processor_ = partial(add_context_processor, store)
-            request_started.connect(add_context_processor_)
-            request_finished.connect(remove_context_processor)
 
         try:
             response = super(TestApp, self).do_request(*args, **kwargs)
         finally:
-            template_rendered.disconnect(store_rendered_template_)
-            request_finished.disconnect(store_session_)
+            template_rendered.disconnect(store_rendered_template)
+            request_finished.disconnect(tear_down_)
+            request_started.disconnect(set_up)
             if message_flashed:
                 message_flashed.disconnect(store_flashed_message)
-            else:
-                request_started.disconnect(add_context_processor_)
-                request_finished.disconnect(remove_context_processor)
 
         response.session = store.get('session', {})
         response.flashes = store.get('flashes', [])
