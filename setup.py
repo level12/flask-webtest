9dcf9d1b68004f26ff2750eb8d04906d0642f50e
aromanovich <anthony.romanovich@gmail.com>
2017-04-04 22:56:44 +0500

    Bump version to 0.0.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Flask-WebTest',
-    version='0.0.8',
+    version='0.0.9',
     url='https://github.com/aromanovich/flask-webtest',
     license='BSD',
     description = 'Utilities for testing Flask applications with WebTest.',
